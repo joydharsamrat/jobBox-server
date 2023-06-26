55a5922e80792bba62bca14ebdce5a38470a046f
joydharsamrat <joydharsamrat@gmail.com>
2023-06-27 00:44:28 +0600

    bug fixed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6qnipai.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,13 +18,13 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db("jobbox");
+    const db = client.db("jobBox");
     const userCollection = db.collection("user");
     const jobCollection = db.collection("job");
 
     app.post("/user", async (req, res) => {
       const user = req.body;
-
+      console.log(user)
       const result = await userCollection.insertOne(user);
 
       res.send(result);
